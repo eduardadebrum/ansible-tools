30457b2734f458570e48058bc5ad795346d39fcb
Eduarda Brum <eduardabrum@MacBook-Pro-de-Eduarda-2.local>
2022-02-15 19:35:58 -0300

    Validate sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,30 @@ echo "============= INSTALLING XCODE ==============="
 xcode-select --install
 sudo xcodebuild -license accept
 
-echo "============= INSTALLING ANSIBLE =============="
+if hash python; then
+    echo " ============= INSTALLING PYTHON =============="
+    /bin/bash -c "$(curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py)"
+    python get-pip.py --user
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install ansible
 
-echo "============= RUNNING ANSIBLE    =============="
+else
+   echo "Do you want to update the python version? (y): "
+   read VAR
+
+   if [[ $VAR == "y" ]]; then
+      echo " ============= UPDATING PYTHON =============="
+    #sudo pip install --upgrade pip
+   fi
+fi
+
+
+if ! hash ansible; then
+    echo " ============= INSTALLING ANSIBLE =============="
+    sudo python get-pip.py
+    sudo python -m pip install ansible
+fi
+
+echo "============= RUNNING ANSIBLE PLAYBOOK   =============="
 # TODO 
-ansible-playbook install.yml
+# ansible-playbook install.yml
 
